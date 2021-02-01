bd71b220055edf12a9a8082230f7fcc51ecb10fd
Liam Tengelis <liam@tengelisconsulting.com>
2021-01-31 16:28:26 -0800

    cleanup

--- integration/tests/adhoc_job_test.py
+++ integration/tests/adhoc_job_test.py
@@ -5,7 +5,6 @@ async def adhoc_job_test1(t):
     schedule_ts = time.time() + 2
     await t.create_adhoc_job(schedule_ts, name="test-job", payload="adhoc-payload-1")
     req = await t.wait_next_req(3)
-    print("received", req)
     t.assert_eq(req, "adhoc-payload-1")
     return
 
